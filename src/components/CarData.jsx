942effb49824df8f5d658ca0494cb946a584bdc7
Taha M <tahamemon543@gmail.com>
2023-11-26 18:26:58 +0500

    pick car, car box and changes

---
+++ src/components/CarData.jsx
@@ -0,0 +1,93 @@
+import Polo from "../images/cars/vw polo.png";
+import Audi from "../images/cars/Audi.png";
+import Toyota from "../images/cars/Toyota camry (1).png";
+import BmwM5 from "../images/cars/bmw m5.png";
+import Benz from "../images/cars/Mercedes.png";
+import Toyota2 from "../images/cars/Toyota  2.png";
+
+export const CAR_DATA = [
+  [
+    {
+      name: "VW Polo",
+      price: "37",
+      img: Polo,
+      model: "Polo Pace 1.0",
+      mark: "Volkswagen",
+      year: "2016-19",
+      doors: "4/5",
+      air: "Yes",
+      transmission: "Manual",
+      fuel: "Diesel",
+    },
+  ],
+  [
+    {
+      name: "Audi",
+      price: "51",
+      img: Audi,
+      model: "Audi",
+      mark: "A6",
+      year: "2018",
+      doors: "4/5",
+      air: "Yes",
+      transmission: "Automatic",
+      fuel: "Gasoline",
+    },
+  ],
+  [
+    {
+      name: "Toyota Camry",
+      price: "30",
+      img: Toyota,
+      model: "Camry",
+      mark: "Toyota",
+      year: "2006",
+      doors: "4/5",
+      air: "Yes",
+      transmission: "Manual",
+      fuel: "Hybrid",
+    },
+  ],
+  [
+    {
+      name: "BMW M5",
+      price: "51",
+      img: BmwM5,
+      model: "M5",
+      mark: "BMW",
+      year: "2017",
+      doors: "4/5",
+      air: "Yes",
+      transmission: "Automatic",
+      fuel: "Diesel",
+    },
+  ],
+  [
+    {
+      name: "Mercedes-Benz",
+      price: "50",
+      img: Benz,
+      model: "Benz",
+      mark: "Mercedes",
+      year: "2018",
+      doors: "4/5",
+      air: "Yes",
+      transmission: "Automatic",
+      fuel: "Diesel",
+    },
+  ],
+  [
+    {
+      name: "Toyota Prado",
+      price: "43",
+      img: Toyota2,
+      model: "Prado Land Cruiser",
+      mark: "Toyota",
+      year: "2018",
+      doors: "4/5",
+      air: "Yes",
+      transmission: "Automatic",
+      fuel: "Gasoline",
+    },
+  ],
+];
