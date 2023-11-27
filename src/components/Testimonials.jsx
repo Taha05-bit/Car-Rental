7e5161d446a119474e24a6a43d357130a5e07808
Taha M <tahamemon543@gmail.com>
2023-11-28 01:20:47 +0500

    Add testimonial and styles

---
+++ src/components/Testimonials.jsx
@@ -0,0 +1,67 @@
+import { IconQuote } from "@tabler/icons-react";
+import Img2 from "../images/Charac/deadpool.png";
+import Img3 from "../images/Charac/deadpool.png";
+
+function Testimonials() {
+  return (
+    <>
+      <section className="testimonials-section">
+        <div className="container">
+          <div className="testimonials-content">
+            <div className="testimonials-content__title">
+              <h4>Reviewed by People</h4>
+              <h2>Client's Testimonials</h2>
+              <p>
+                Discover the positive impact we've made on the our clients by
+                reading through their testimonials. Our clients have experienced
+                our service and results, and they're eager to share their
+                positive experiences with you.
+              </p>
+            </div>
+
+            <div className="all-testimonials">
+              <div className="all-testimonials__box">
+                <span className="quotes-icon">
+                  <IconQuote width={60} height={60} />
+                </span>
+                <p>
+                  "These guys have got some crazy prices here!"
+                </p>
+                <div className="all-testimonials__box__name">
+                  <div className="all-testimonials__box__name__profile">
+                    <img src={Img2} alt="user_img" />
+                    <span>
+                      <h4>Dead Pool</h4>
+                      <p>Dublin</p>
+                    </span>
+                  </div>
+                </div>
+              </div>
+
+              <div className="all-testimonials__box box-2">
+                <span className="quotes-icon">
+                  <IconQuote width={60} height={60} />
+                </span>
+                <p>
+                  "The car was in great condition and made our trip even better.
+                  Highly recommend for this car rental website!"
+                </p>
+                <div className="all-testimonials__box__name">
+                  <div className="all-testimonials__box__name__profile">
+                    <img src={Img3} alt="user_img" />
+                    <span>
+                      <h4>Deap Pool's brother</h4>
+                      <p>Cork</p>
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Testimonials;
