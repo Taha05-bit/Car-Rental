25d48c41e218fd0f040205045f3ca9b7becacdec
Taha M <tahamemon543@gmail.com>
2023-11-28 01:24:59 +0500

    Add models sections and styles

---
+++ src/Pages/Models.jsx
@@ -0,0 +1,298 @@
+import Footer from "../components/Footer";
+import HeroPages from "../components/HeroPage";
+import CarImg1 from "../images/cars/audi3 (1).jpg";
+import CarImg2 from "../images/cars/polo 2.jpg";
+import CarImg3 from "../images/cars/camry.jpg";
+import CarImg4 from "../images/cars/m4.jpg";
+import CarImg5 from "../images/cars/mercedes 2.jpg";
+import CarImg6 from "../images/cars/prado.jpg";
+import { Link } from "react-router-dom";
+import { IconCar, IconPhone, IconStar } from "@tabler/icons-react";
+import BackToTop from "../components/BackToTop";
+
+function Models() {
+  return (
+    <>
+      <section className="models-section">
+        <HeroPages name="Vehicle Models" />
+        <div className="container">
+          <div className="models-div">
+            <div className="models-div__box">
+              <div className="models-div__box__img">
+                <img src={CarImg1} alt="car_img" />
+                <div className="models-div__box__descr">
+                  <div className="models-div__box__descr__name-price">
+                    <div className="models-div__box__descr__name-price__name">
+                      <p>Audi A6</p>
+                      <span>
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                      </span>
+                    </div>
+                    <div className="models-div__box__descr__name-price__price">
+                      <h4>$51</h4>
+                      <p>per day</p>
+                    </div>
+                  </div>
+                  <div className="models-div__box__descr__name-price__details">
+                    <span>
+                      <IconCar /> &nbsp; Audi
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      4/5 &nbsp; <IconCar />
+                    </span>
+                    <span>
+                      <IconCar /> &nbsp; Automatic
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      Gasoline &nbsp; <IconCar />
+                    </span>
+                  </div>
+                  <div className="models-div__box__descr__name-price__btn">
+                    <Link onClick={() => window.scrollTo(0, 0)} to="/">
+                      Book Ride
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="models-div__box">
+              <div className="models-div__box__img">
+                <img src={CarImg2} alt="car_img" />
+                <div className="models-div__box__descr">
+                  <div className="models-div__box__descr__name-price">
+                    <div className="models-div__box__descr__name-price__name">
+                      <p>Polo</p>
+                      <span>
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                      </span>
+                    </div>
+                    <div className="models-div__box__descr__name-price__price">
+                      <h4>$37</h4>
+                      <p>per day</p>
+                    </div>
+                  </div>
+                  <div className="models-div__box__descr__name-price__details">
+                    <span>
+                      <IconCar /> &nbsp; VW
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      4/5 &nbsp; <IconCar />
+                    </span>
+                    <span>
+                      <IconCar /> &nbsp; Manual
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      Diesel &nbsp; <IconCar />
+                    </span>
+                  </div>
+                  <div className="models-div__box__descr__name-price__btn">
+                    <Link onClick={() => window.scrollTo(0, 0)} to="/">
+                      Book Ride
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="models-div__box">
+              <div className="models-div__box__img">
+                <img src={CarImg3} alt="car_img" />
+                <div className="models-div__box__descr">
+                  <div className="models-div__box__descr__name-price">
+                    <div className="models-div__box__descr__name-price__name">
+                      <p>Toyota</p>
+                      <span>
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                      </span>
+                    </div>
+                    <div className="models-div__box__descr__name-price__price">
+                      <h4>$30</h4>
+                      <p>per day</p>
+                    </div>
+                  </div>
+                  <div className="models-div__box__descr__name-price__details">
+                    <span>
+                      <IconCar /> &nbsp; Camry
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      4/5 &nbsp; <IconCar />
+                    </span>
+                    <span>
+                      <IconCar /> &nbsp; Manual
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      Hybrid &nbsp; <IconCar />
+                    </span>
+                  </div>
+                  <div className="models-div__box__descr__name-price__btn">
+                    <Link onClick={() => window.scrollTo(0, 0)} to="/">
+                      Book Ride
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="models-div__box">
+              <div className="models-div__box__img">
+                <img src={CarImg4} alt="car_img" />
+                <div className="models-div__box__descr">
+                  <div className="models-div__box__descr__name-price">
+                    <div className="models-div__box__descr__name-price__name">
+                      <p>BMW</p>
+                      <span>
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                      </span>
+                    </div>
+                    <div className="models-div__box__descr__name-price__price">
+                      <h4>$51</h4>
+                      <p>per day</p>
+                    </div>
+                  </div>
+                  <div className="models-div__box__descr__name-price__details">
+                    <span>
+                      <IconCar /> &nbsp; M5
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      4/5 &nbsp; <IconCar />
+                    </span>
+                    <span>
+                      <IconCar /> &nbsp; Automatic
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      Diesel &nbsp; <IconCar />
+                    </span>
+                  </div>
+                  <div className="models-div__box__descr__name-price__btn">
+                    <Link onClick={() => window.scrollTo(0, 0)} to="/">
+                      Book Ride
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="models-div__box">
+              <div className="models-div__box__img">
+                <img src={CarImg5} alt="car_img" />
+                <div className="models-div__box__descr">
+                  <div className="models-div__box__descr__name-price">
+                    <div className="models-div__box__descr__name-price__name">
+                      <p>Mercedes</p>
+                      <span>
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                      </span>
+                    </div>
+                    <div className="models-div__box__descr__name-price__price">
+                      <h4>$50</h4>
+                      <p>per day</p>
+                    </div>
+                  </div>
+                  <div className="models-div__box__descr__name-price__details">
+                    <span>
+                      <IconCar /> &nbsp; Benz GLE
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      4/5 &nbsp; <IconCar />
+                    </span>
+                    <span>
+                      <IconCar /> &nbsp; Automatic
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      Diesel &nbsp; <IconCar />
+                    </span>
+                  </div>
+                  <div className="models-div__box__descr__name-price__btn">
+                    <Link onClick={() => window.scrollTo(0, 0)} to="/">
+                      Book Ride
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="models-div__box">
+              <div className="models-div__box__img">
+                <img src={CarImg6} alt="car_img" />
+                <div className="models-div__box__descr">
+                  <div className="models-div__box__descr__name-price">
+                    <div className="models-div__box__descr__name-price__name">
+                      <p>Toyota</p>
+                      <span>
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                        <IconStar width={15} height={15} />
+                      </span>
+                    </div>
+                    <div className="models-div__box__descr__name-price__price">
+                      <h4>$43</h4>
+                      <p>per day</p>
+                    </div>
+                  </div>
+                  <div className="models-div__box__descr__name-price__details">
+                    <span>
+                      <IconCar /> &nbsp; Prado
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      4/5 &nbsp; <IconCar />
+                    </span>
+                    <span>
+                      <IconCar /> &nbsp; Automatic
+                    </span>
+                    <span style={{ textAlign: "right" }}>
+                      Gasoline &nbsp; <IconCar />
+                    </span>
+                  </div>
+                  <div className="models-div__box__descr__name-price__btn">
+                    <Link onClick={() => window.scrollTo(0, 0)} to="/">
+                      Book Ride
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="book-banner">
+          <div className="book-banner__overlay"></div>
+          <div className="container">
+            <div className="text-content">
+              <h2>Wanna book a car? Get in touch with us.</h2>
+              <span>
+                <IconPhone width={40} height={40} />
+                <h3>123-456-7869</h3>
+              </span>
+            </div>
+          </div>
+        </div>
+        <BackToTop />
+        <Footer />
+      </section>
+    </>
+  );
+}
+
+export default Models;
