0172bed7547268b41961e6594e284c89b7c083a4
Taha M <tahamemon543@gmail.com>
2023-11-28 01:23:09 +0500

    Home page update

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,30 @@
 import React from "react";
-import Navbar from "../components/Navbar";
+import BackToTop from "../components/BackToTop";
 import Hero from "../components/Hero";
-
+import BookCar from "../components/BookCar";
+import PlanTrip from "../components/TripPlan";
+import PickCar from "../components/PickCar";
+import Banner from "../components/Banner";
+import ChooseUs from "../components/WhyChooseUs";
+import Testimonials from "../components/Testimonials";
+import Faq from "../components/Faq";
+import Download from "../components/DownloadApp";
+import Footer from "../components/Footer";
 
 function Home(){
     return(
         <div>
         <Hero />
+        <BookCar />
+        <PlanTrip />
+        <PickCar />
+        <Banner />
+        <ChooseUs />
+        <Testimonials />
+        <Faq />
+        <Download />
+        <Footer />
+        <BackToTop/>
         </div>
     )
 }
